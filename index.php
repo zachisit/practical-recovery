291fd84cae2da78ff7cc791554996279b05a089b
Zachary Smith <info@zachis.it>
2017-11-27 07:30:42 -0500

    introducing base theme

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The main template file
+ *
+ * Serves as landing page for theme
+ *
+ * @package practical-recovery
+ */
+get_header(); ?>
+
+<main>
+
+</main>
+
+<?php get_footer(); ?>
